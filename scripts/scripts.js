b4d05eb96025c352f9944824313e11c8fc4ca76a
yelchuriaakanksha <aakanksha.yelchuri@gmail.com>
2019-01-09 16:17:43 +0530

    save files

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+console.log('Nothing here so far, but thank you for checking =)');
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
